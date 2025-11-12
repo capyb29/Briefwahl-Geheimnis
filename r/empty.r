af32f7db20164d6e3574687c4e47666a76da2bb9
omardaus <125575247+omardaus@users.noreply.github.com>
2025-11-12 17:02:54 +0100

    rename

--- r/empty.r
+++
@@ -1,7 +0,0 @@
-
-btw2025 = read.csv("../data/btw2025_bezirk.csv", sep = ";")
-
-btw2025 = btw2025[,colSums(is.na(btw2025)) == 0] #removes columns with only NA values
-btw2025 = btw2025[-(1:4),] #ersten 4 zeilen löschen
-colnames(btw2025) = btw2025[1,] #erste zeile als spaltennamen
-btw2025 = btw2025[-1,] #erste zeile löschen
