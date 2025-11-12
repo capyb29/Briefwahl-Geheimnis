a52e9a2dce4204171e6149d882ce918b43d1f283
omardaus <125575247+omardaus@users.noreply.github.com>
2025-11-12 19:21:53 +0100

    create table with funtion from prev commit and save to csv file

--- r/main.r
+++ r/main.r
@@ -10,15 +10,20 @@ loadcsv = function(file, uselessrows) {
   return(df)
 }
 
+savecsv = function(df, file) {
+  write.csv2(df, paste0("../data/", file), row.names = TRUE)
+}
+
 btw2025 = loadcsv("btw2025_bezirk.csv", 4)
 
 btw2021kreis = loadcsv("btw2021kreis.csv", 3)
 btw2021kreis = btw2021kreis[-1,] # offensichtlich, dass alles erststimmen bei der kreiswahl sind
 
 btwUrneBrief = loadcsv("btw_urne-brief_1957_2025.csv", 3)
-btwUrneBrief = btwUrneBrief[btwUrneBrief$`Jahr der Wahl` >= 2017, ]
+btwUrneBrief = btwUrneBrief[btwUrneBrief$`Jahr der Wahl` >= 1994, ]
 rownames(btwUrneBrief) = NULL
 
+# convert cols to numeric
 btwUrneBrief[,4:ncol(btwUrneBrief)] = lapply(btwUrneBrief[,4:ncol(btwUrneBrief)], function(x) as.numeric(gsub("\\.", "", btwUrneBrief$Wählende)))
 
 anzahlBrief = function(Land, Jahr) {
@@ -30,5 +35,14 @@ anzahlBrief = function(Land, Jahr) {
   }
 }
 
-anteil = anzahlBrief("BW", 2025)
-cat(anteil * 100)
+AnteileBriefwähler = data.frame(
+  Land = unique(btwUrneBrief$Land),
+  Anteil_Brief_2017 = sapply(unique(btwUrneBrief$Land), function(x) anzahlBrief(x, 2017) * 100),
+  Anteil_Brief_2021 = sapply(unique(btwUrneBrief$Land), function(x) anzahlBrief(x, 2021) * 100),
+  Anteil_Brief_2025 = sapply(unique(btwUrneBrief$Land), function(x) anzahlBrief(x, 2025) * 100)
+)
+rownames(AnteileBriefwähler) = AnteileBriefwähler$Land
+AnteileBriefwähler$Land = NULL
+
+savecsv(AnteileBriefwähler, "Anteile_Briefwaehler.csv")
+
