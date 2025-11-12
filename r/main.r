1a37624fca963d8e1cbe5e237b9cbfd0fd492908
omardaus <125575247+omardaus@users.noreply.github.com>
2025-11-12 18:44:43 +0100

    implementierung funktion zur bestimmung des prozentualen anteil der briefwähler für angegebenes land und jahr

--- r/main.r
+++ r/main.r
@@ -11,5 +11,24 @@ loadcsv = function(file, uselessrows) {
 }
 
 btw2025 = loadcsv("btw2025_bezirk.csv", 4)
+
 btw2021kreis = loadcsv("btw2021kreis.csv", 3)
 btw2021kreis = btw2021kreis[-1,] # offensichtlich, dass alles erststimmen bei der kreiswahl sind
+
+btwUrneBrief = loadcsv("btw_urne-brief_1957_2025.csv", 3)
+btwUrneBrief = btwUrneBrief[btwUrneBrief$`Jahr der Wahl` >= 2017, ]
+rownames(btwUrneBrief) = NULL
+
+btwUrneBrief[,4:ncol(btwUrneBrief)] = lapply(btwUrneBrief[,4:ncol(btwUrneBrief)], function(x) as.numeric(gsub("\\.", "", btwUrneBrief$Wählende)))
+
+anzahlBrief = function(Land, Jahr) {
+  df = btwUrneBrief[btwUrneBrief$Land == Land & btwUrneBrief$`Jahr der Wahl` == Jahr, ]
+  if (nrow(df) == 2) {
+    a = df[df$Wahlbezirksart == "Brief", "Wählende"] /
+             sum(df$Wählende)
+    return(round(a, digits=3))
+  }
+}
+
+anteil = anzahlBrief("BW", 2025)
+cat(anteil * 100)
