7e2059582a6fd71a84a471d13f00c4fdf037d4aa
omardaus <125575247+omardaus@users.noreply.github.com>
2025-11-13 14:18:25 +0100

    minor

--- r/main.r
+++ r/main.r
@@ -15,4 +15,8 @@ savecsv = function(df, file) {
 }
 
 bund17 = loadcsv("2017_Bund_Partei_Brief_Urne_Alter_Geschlecht.csv", 10)
-kreis17 = loadcsv("2017_Urne_Brief_Wahlkreise_Parteien.csv", 4)
+kreis17 = loadcsv("2017_Urne_Brief_Wahlkreise_Parteien.csv", 3)
+bund21 = loadcsv("2021_Bund_Partei_Brief_Urne_Alter_Geschlecht.csv", 11)
+kreis21 = loadcsv("2021_Urne_Brief_Wahlkreise_Parteien.csv", 3)
+bund25 = loadcsv("2025_Bund_Partei_Brief_Urne_Alter_Geschlecht.csv", 11)
+kreis25 = loadcsv("2025_Urne_Brief_Wahlkreise_Parteien.csv", 3)
