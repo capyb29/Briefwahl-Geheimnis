c1dfe9da288c18d85558af25520584aa17dafa95
omardaus <125575247+omardaus@users.noreply.github.com>
2025-11-13 14:03:30 +0100

    für philipp

--- r/main.r
+++ r/main.r
@@ -1,12 +1,12 @@
 
 
 loadcsv = function(file, uselessrows) {
-  df = read.csv(paste0("../data/", file), sep = ";")
+  df = read.csv(paste0("../data/", file), skip=uselessrows, sep=";")
   df = df[,colSums(is.na(df)) == 0]
-  df = df[-(1:uselessrows),]
   colnames(df) = df[1,]
   df = df[-1,]
   rownames(df) = NULL
+  print(paste0("NA Values: ", any(is.na(df))))
   return(df)
 }
 
@@ -14,4 +14,5 @@ savecsv = function(df, file) {
   write.csv2(df, paste0("../data/", file), row.names = TRUE)
 }
 
-bund17 = loadcsv("2017_Bund_Partei_Brief_Urne_Alter_Geschlecht.csv", 3)
+bund17 = loadcsv("2017_Bund_Partei_Brief_Urne_Alter_Geschlecht.csv", 10)
+kreis17 = loadcsv("2017_Urne_Brief_Wahlkreise_Parteien.csv", 4)
