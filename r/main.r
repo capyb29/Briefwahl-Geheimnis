6b82ecaffa7f903e9c763079962d539a67394e5b
omardaus <125575247+omardaus@users.noreply.github.com>
2025-11-17 12:16:21 +0100

    bund combined fixed

--- r/main.r
+++ r/main.r
@@ -6,7 +6,12 @@ kreis17 = loadcsv("2017_Urne_Brief_Wahlkreise_Parteien.csv", 3)
 bund21 = loadcsv("2021_Bund_Partei_Brief_Urne_Alter_Geschlecht.csv", 11)
 kreis21 = loadcsv("2021_Urne_Brief_Wahlkreise_Parteien.csv", 3)
 bund25 = loadcsv("2025_Bund_Partei_Brief_Urne_Alter_Geschlecht.csv", 11)
+# Bei 2025 m|d|o Geschlecht zu m geändert 
+bund25$Geschlecht[bund25$Geschlecht == "m|d|o"] = "m"
 kreis25 = loadcsv("2025_Urne_Brief_Wahlkreise_Parteien.csv", 3)
+# Spaltennamen anpassen
+names(kreis25)[names(kreis25) == "Wählende"] = "Wähler"
+names(kreis21)[names(kreis21) == "Wählende"] = "Wähler"
 
 
 # returns total amount of voters for given parameters
@@ -27,7 +32,7 @@ briefAnteile = function() {
       männlich = c(
         getWählerAmount(bund17, "Summe", "m", "Summe", "2"),
         getWählerAmount(bund21, "Summe", "m", "Summe", "2"),
-        getWählerAmount(bund25, "Summe", "m|d|o", "Summe", "2")
+        getWählerAmount(bund25, "Summe", "m", "Summe", "2")
       ),
       weiblich = c(
         getWählerAmount(bund17, "Summe", "w", "Summe", "2"),
@@ -91,13 +96,17 @@ bundDatenBereinigen = function(df,df2,df3) {
   df = df[df$`Erst-/Zweitstimme` == 2,]
   df2 = df2[df2$`Erst-/Zweitstimme` == 2,]
   df3 = df3[df3$`Erst-/Zweitstimme` == 2,]
+  df$Jahr = 2017
+  df2$Jahr = 2021
+  df3$Jahr = 2025
   vec_tmp = c(colnames(df),colnames(df2),colnames(df3))
   vec_tmp = unique(vec_tmp)
+  vec_tmp = c("Jahr", vec_tmp[vec_tmp != "Jahr"])
   df_new = data.frame(matrix(ncol = length(vec_tmp), nrow = 0))
   colnames(df_new) = vec_tmp
   df_new = rbind_fill_na(df_new, df)
   df_new = rbind_fill_na(df_new, df2)
-  df_new = rbind_fill_na(df_new, df3)
+  rownames(df_new) = NULL
   return(df_new)
 }
 
@@ -105,9 +114,6 @@ bundDatenBereinigen = function(df,df2,df3) {
 # Anteil der Briefwähler pro Wahljahr
 übersichtAnteilBriefwähler = briefAnteile()
 savecsv(übersichtAnteilBriefwähler, "Anteil_Briefwähler.csv")
-
-# Bei 2025 m|d|o Geschlecht zu m geändert 
-bund25$Geschlecht[bund25$Geschlecht == "m|d|o"] = "m"
 
 kreis17_zweitstimmen = getZweitstimmenKreis(kreis17)
 kreis17_zweitstimmen = correctBundesLänder(kreis17_zweitstimmen)
@@ -120,10 +126,23 @@ kreis25_zweitstimmen = correctBundesLänder(kreis25_zweitstimmen)
 savecsv(kreis25_zweitstimmen, "Kreis25_Zweitstimmen.csv")
 
 #Anteile der Briefwähler in den Ländern
-briefwähler_länder = kreis17_zweitstimmen %>% group_by(Land) %>% 
-       summarise(
-         Wahlberechtigte = sum(Wahlberechtigte), 
-         Wähler = sum(Wähler),
-         Anteil_Briefwähler = round(sum(Wähler) / sum(Wahlberechtigte), 3) * 100
-       )
+briefwähler_länder = function(df) {
+   res = df %>% group_by(Land) %>% 
+    summarise(
+      Wahlberechtigte = sum(Wahlberechtigte), 
+      Wähler = sum(Wähler),
+      Anteil_Briefwähler = round(sum(Wähler) / sum(Wahlberechtigte), 3) * 100
+    )
+   
+   summary = list(Land = "Bund", Wahlberechtigte = sum(res$Wahlberechtigte), Wähler = sum(res$Wähler), Anteil_Briefwähler = round(sum(res$Wähler) / sum(res$Wahlberechtigte), 3)*100)
+   summary = as.data.frame(summary)
+   #res = rbind(res, summary)
+  return(res)
+}
+briefwähler_länder17 = briefwähler_länder(kreis17_zweitstimmen)
+briefwähler_länder21 = briefwähler_länder(kreis21_zweitstimmen)
+briefwähler_länder25 = briefwähler_länder(kreis25_zweitstimmen)
+
+
 bund = bundDatenBereinigen(bund17, bund21, bund25)
+savecsv(bund, "Bund_combined.csv")
