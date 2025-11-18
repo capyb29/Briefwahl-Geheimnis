23f6d899f96085b31af8f55a5a33d303a4a249c6
Philipp Weingart <w2005@gmx.de>
2025-11-18 15:43:04 +0100

    moved all relevant functions to the lib.r file

--- r/lib.R
+++ r/lib.R
@@ -90,3 +90,178 @@ rbind_fill_na = function(df_big, df_small) {
 pct = function(num) {
   return(round(num * 100, 1))
 }
+
+getWählerAmount = function(df,
+                           bezirksart,
+                           geschlecht,
+                           geburtsjahr,
+                           ez) {
+  df = df[df$Bezirksart == bezirksart &
+            paste0(df$Geschlecht, "test") == paste0(geschlecht, "test") &
+            df$Geburtsjahresgruppe == geburtsjahr &
+            df$`Erst-/Zweitstimme` == ez, "Summe"]
+  return(as.numeric(df))
+}
+
+briefAnteile = function() {
+  df = data.frame(
+    Jahr = c("2017", "2021", "2025"),
+    `Wähler gesamt` = c(
+      getWählerAmount (bund17, "Summe", "Summe", "Summe", "2"),
+      getWählerAmount(bund21, "Summe", "Summe", "Summe", "2"),
+      getWählerAmount(bund25, "Summe", "Summe", "Summe", "2")
+    ),
+    männlich = c(
+      getWählerAmount(bund17, "Summe", "m", "Summe", "2"),
+      getWählerAmount(bund21, "Summe", "m", "Summe", "2"),
+      getWählerAmount(bund25, "Summe", "m", "Summe", "2")
+    ),
+    weiblich = c(
+      getWählerAmount(bund17, "Summe", "w", "Summe", "2"),
+      getWählerAmount(bund21, "Summe", "w", "Summe", "2"),
+      getWählerAmount(bund25, "Summe", "w", "Summe", "2")
+    ),
+    Briefwähler = c(
+      getWählerAmount(bund17, "Brief", "Summe", "Summe", "2"),
+      getWählerAmount(bund21, "Brief", "Summe", "Summe", "2"),
+      getWählerAmount(bund25, "Brief", "Summe", "Summe", "2")
+    ),
+    Urnenwähler = c(
+      getWählerAmount(bund17, "Urne", "Summe", "Summe", "2"),
+      getWählerAmount(bund21, "Urne", "Summe", "Summe", "2"),
+      getWählerAmount(bund25, "Urne", "Summe", "Summe", "2")
+    ),
+    `Anteil der Briefwähler` = c(
+      getWählerAmount(bund17, "Brief", "Summe", "Summe", "2") / getWählerAmount(bund17, "Summe", "Summe", "Summe", "2"),
+      getWählerAmount(bund21, "Brief", "Summe", "Summe", "2") / getWählerAmount(bund21, "Summe", "Summe", "Summe", "2"),
+      getWählerAmount(bund25, "Brief", "Summe", "Summe", "2") / getWählerAmount(bund25, "Summe", "Summe", "Summe", "2")
+    ),
+    `Anteil der Urnenwähler` = c(
+      getWählerAmount(bund17, "Urne", "Summe", "Summe", "2") / getWählerAmount(bund17, "Summe", "Summe", "Summe", "2"),
+      getWählerAmount(bund21, "Urne", "Summe", "Summe", "2") / getWählerAmount(bund21, "Summe", "Summe", "Summe", "2"),
+      getWählerAmount(bund25, "Urne", "Summe", "Summe", "2") / getWählerAmount(bund25, "Summe", "Summe", "Summe", "2")
+    )
+  )
+  rownames(df) = df$Jahr
+  df$Jahr = NULL
+  return((df))
+}
+
+# Daten Bereinigen und Zusammenfassen
+bundDatenBereinigen = function(df, df2, df3) {
+  df = df[df$`Erst-/Zweitstimme` == 2, ]
+  df2 = df2[df2$`Erst-/Zweitstimme` == 2, ]
+  df3 = df3[df3$`Erst-/Zweitstimme` == 2, ]
+  names(df3)[names(df3) == "Die Linke"] = "DIE LINKE"
+  df$Jahr = 2017
+  df2$Jahr = 2021
+  df3$Jahr = 2025
+  vec_tmp = c(colnames(df), colnames(df2), colnames(df3))
+  vec_tmp = unique(vec_tmp)
+  vec_tmp = c("Jahr", vec_tmp[vec_tmp != "Jahr"])
+  df_new = data.frame(matrix(ncol = length(vec_tmp), nrow = 0))
+  colnames(df_new) = vec_tmp
+  df_new = rbind_fill_na(df_new, df)
+  df_new = rbind_fill_na(df_new, df2)
+  df_new = rbind_fill_na(df_new, df3)
+  df_new$`Erst-/Zweitstimme` = NULL
+  rownames(df_new) = NULL
+  df_new$Land = NULL
+  return(df_new)
+}
+
+kreisdatenBereinigen = function(df, df2, df3) {
+  df$Jahr = 2017
+  df2$Jahr = 2021
+  df3$Jahr = 2025
+  
+  names(df2) = sub("du.", "du", names(df2))
+  
+  names(df3) = sub(" - Zweitstimmen$", "", names(df3))
+  names(df3) = sub("Nummer", "Nr.", names(df3))
+  names(df3) = sub("Die Linke", "DIE LINKE", names(df3))
+  
+  vec_tmp = c(colnames(df), colnames(df2), colnames(df3))
+  vec_tmp = unique(vec_tmp)
+  vec_tmp = c("Jahr", vec_tmp[vec_tmp != "Jahr"])
+  vec_tmp = sub(".1", "", vec_tmp)
+  df_new = data.frame(matrix(ncol = length(vec_tmp), nrow = 0))
+  colnames(df_new) = vec_tmp
+  df_new = rbind_fill_na(df_new, df)
+  df_new = rbind_fill_na(df_new, df2)
+  df_new = rbind_fill_na(df_new, df3)
+  rownames(df_new) = NULL
+  
+  return(df_new)
+}
+
+#Anteile der Briefwähler in den Ländern
+briefwähler_länder = function(df) {
+  res = df %>% group_by(Land) %>%
+    summarise(
+      Wahlberechtigte = sum(Wahlberechtigte),
+      Wähler = sum(Wähler),
+      Anteil_Briefwähler = round(sum(Wähler) / sum(Wahlberechtigte), 3) * 100
+    )
+  
+  summary = list(
+    Land = "Bund",
+    Wahlberechtigte = sum(res$Wahlberechtigte),
+    Wähler = sum(res$Wähler),
+    Anteil_Briefwähler = round(sum(res$Wähler) / sum(res$Wahlberechtigte), 3) *
+      100
+  )
+  summary = as.data.frame(summary)
+  #res = rbind(res, summary)
+  return(res)
+}
+
+# ANALYSEN
+kreisWahlberechtigteByGroup = function(filter_list) {
+  df = kreis_daten_gesamt
+  
+  for (col in names(filter_list)) {
+    val = filter_list[[col]]
+    df = df %>% filter(.data[[col]] == val)
+  }
+  
+  result = df %>%
+    arrange(`Wahlkreis-Nr.`) %>%
+    group_by(`Wahlkreis-Nr.`) %>%
+    slice_head(n = 1) %>%
+    ungroup()
+  
+  sum(result$Wahlberechtigte)
+}
+
+bundSumByGroup = function(filter_list) {
+  df = bund_komplett
+  for (col in colnames(filter_list)) {
+    val = filter_list[[col]]
+    df = df %>% filter(.data[[col]] == val)
+  }
+  return(sum(df$Summe))
+}
+
+bundAnalyse = function(..., group = "Jahr") {
+  args = list(...)
+  
+  res = bund_komplett
+  for (col in names(args)) {
+    val = args[[col]]
+    res = res %>% filter(.data[[col]] == val)
+  }
+  
+  res = res %>% group_by(across(all_of(group))) %>%
+    summarise(
+      Bezirksart_Anteil = pct(sum(Summe) / sum(bund_komplett[bund_komplett$Jahr == Jahr, "Summe"])),
+      CDU_CSU = pct((sum(CDU) + sum(CSU)) / bundSumByGroup(cur_group())),
+      SPD = pct(sum(SPD) / bundSumByGroup(cur_group())),
+      GRÜNE = pct(sum(GRÜNE) / bundSumByGroup(cur_group())),
+      LINKE = pct(sum(`DIE LINKE`) / bundSumByGroup(cur_group())),
+      FDP = pct(sum(FDP) / bundSumByGroup(cur_group())),
+      AFD = pct(sum(AfD) / bundSumByGroup(cur_group())),
+      Sonstige = pct(sum(Sonstige) / bundSumByGroup(cur_group()))
+    )
+  return(res)
+}
--- r/main.r
+++ r/main.r
@@ -15,65 +15,6 @@ kreis25 = loadcsv("2025_Urne_Brief_Wahlkreise_Parteien.csv", 3)
 names(kreis25)[names(kreis25) == "Wählende"] = "Wähler"
 names(kreis21)[names(kreis21) == "Wählende"] = "Wähler"
 
-
-# returns total amount of voters for given parameters
-getWählerAmount = function(df,
-                           bezirksart,
-                           geschlecht,
-                           geburtsjahr,
-                           ez) {
-  #only for bund dataframes
-  df = df[df$Bezirksart == bezirksart &
-            paste0(df$Geschlecht, "test") == paste0(geschlecht, "test") &
-            df$Geburtsjahresgruppe == geburtsjahr &
-            df$`Erst-/Zweitstimme` == ez, "Summe"]
-  return(as.numeric(df))
-}
-
-briefAnteile = function() {
-  df = data.frame(
-    Jahr = c("2017", "2021", "2025"),
-    `Wähler gesamt` = c(
-      getWählerAmount (bund17, "Summe", "Summe", "Summe", "2"),
-      getWählerAmount(bund21, "Summe", "Summe", "Summe", "2"),
-      getWählerAmount(bund25, "Summe", "Summe", "Summe", "2")
-    ),
-    männlich = c(
-      getWählerAmount(bund17, "Summe", "m", "Summe", "2"),
-      getWählerAmount(bund21, "Summe", "m", "Summe", "2"),
-      getWählerAmount(bund25, "Summe", "m", "Summe", "2")
-    ),
-    weiblich = c(
-      getWählerAmount(bund17, "Summe", "w", "Summe", "2"),
-      getWählerAmount(bund21, "Summe", "w", "Summe", "2"),
-      getWählerAmount(bund25, "Summe", "w", "Summe", "2")
-    ),
-    Briefwähler = c(
-      getWählerAmount(bund17, "Brief", "Summe", "Summe", "2"),
-      getWählerAmount(bund21, "Brief", "Summe", "Summe", "2"),
-      getWählerAmount(bund25, "Brief", "Summe", "Summe", "2")
-    ),
-    Urnenwähler = c(
-      getWählerAmount(bund17, "Urne", "Summe", "Summe", "2"),
-      getWählerAmount(bund21, "Urne", "Summe", "Summe", "2"),
-      getWählerAmount(bund25, "Urne", "Summe", "Summe", "2")
-    ),
-    `Anteil der Briefwähler` = c(
-      getWählerAmount(bund17, "Brief", "Summe", "Summe", "2") / getWählerAmount(bund17, "Summe", "Summe", "Summe", "2"),
-      getWählerAmount(bund21, "Brief", "Summe", "Summe", "2") / getWählerAmount(bund21, "Summe", "Summe", "Summe", "2"),
-      getWählerAmount(bund25, "Brief", "Summe", "Summe", "2") / getWählerAmount(bund25, "Summe", "Summe", "Summe", "2")
-    ),
-    `Anteil der Urnenwähler` = c(
-      getWählerAmount(bund17, "Urne", "Summe", "Summe", "2") / getWählerAmount(bund17, "Summe", "Summe", "Summe", "2"),
-      getWählerAmount(bund21, "Urne", "Summe", "Summe", "2") / getWählerAmount(bund21, "Summe", "Summe", "Summe", "2"),
-      getWählerAmount(bund25, "Urne", "Summe", "Summe", "2") / getWählerAmount(bund25, "Summe", "Summe", "Summe", "2")
-    )
-  )
-  rownames(df) = df$Jahr
-  df$Jahr = NULL
-  return((df))
-}
-
 correctBundesLänder = function(df) {
   bundeslaender <- c(
     HH = "Hamburg",
@@ -100,55 +41,6 @@ correctBundesLänder = function(df) {
   return(df)
 }
 
-# Daten Bereinigen und Zusammenfassen
-bundDatenBereinigen = function(df, df2, df3) {
-  df = df[df$`Erst-/Zweitstimme` == 2, ]
-  df2 = df2[df2$`Erst-/Zweitstimme` == 2, ]
-  df3 = df3[df3$`Erst-/Zweitstimme` == 2, ]
-  names(df3)[names(df3) == "Die Linke"] = "DIE LINKE"
-  df$Jahr = 2017
-  df2$Jahr = 2021
-  df3$Jahr = 2025
-  vec_tmp = c(colnames(df), colnames(df2), colnames(df3))
-  vec_tmp = unique(vec_tmp)
-  vec_tmp = c("Jahr", vec_tmp[vec_tmp != "Jahr"])
-  df_new = data.frame(matrix(ncol = length(vec_tmp), nrow = 0))
-  colnames(df_new) = vec_tmp
-  df_new = rbind_fill_na(df_new, df)
-  df_new = rbind_fill_na(df_new, df2)
-  df_new = rbind_fill_na(df_new, df3)
-  df_new$`Erst-/Zweitstimme` = NULL
-  rownames(df_new) = NULL
-  df_new$Land = NULL
-  return(df_new)
-}
-
-kreisdatenBereinigen = function(df, df2, df3) {
-  df$Jahr = 2017
-  df2$Jahr = 2021
-  df3$Jahr = 2025
-  
-  names(df2) = sub("du.", "du", names(df2))
-  
-  names(df3) = sub(" - Zweitstimmen$", "", names(df3))
-  names(df3) = sub("Nummer", "Nr.", names(df3))
-  names(df3) = sub("Die Linke", "DIE LINKE", names(df3))
-  
-  vec_tmp = c(colnames(df), colnames(df2), colnames(df3))
-  vec_tmp = unique(vec_tmp)
-  vec_tmp = c("Jahr", vec_tmp[vec_tmp != "Jahr"])
-  vec_tmp = sub(".1", "", vec_tmp)
-  df_new = data.frame(matrix(ncol = length(vec_tmp), nrow = 0))
-  colnames(df_new) = vec_tmp
-  df_new = rbind_fill_na(df_new, df)
-  df_new = rbind_fill_na(df_new, df2)
-  df_new = rbind_fill_na(df_new, df3)
-  rownames(df_new) = NULL
-  
-  return(df_new)
-}
-
-
 # Anteil der Briefwähler pro Wahljahr
 übersichtAnteilBriefwähler = briefAnteile()
 savecsv(übersichtAnteilBriefwähler, "Anteil_Briefwähler.csv")
@@ -163,27 +55,6 @@ kreis25_zweitstimmen = getZweitstimmenKreis(kreis25)
 kreis25_zweitstimmen = correctBundesLänder(kreis25_zweitstimmen)
 savecsv(kreis25_zweitstimmen, "Kreis25_Zweitstimmen.csv")
 
-#Anteile der Briefwähler in den Ländern
-briefwähler_länder = function(df) {
-  res = df %>% group_by(Land) %>%
-    summarise(
-      Wahlberechtigte = sum(Wahlberechtigte),
-      Wähler = sum(Wähler),
-      Anteil_Briefwähler = round(sum(Wähler) / sum(Wahlberechtigte), 3) * 100
-    )
-  
-  summary = list(
-    Land = "Bund",
-    Wahlberechtigte = sum(res$Wahlberechtigte),
-    Wähler = sum(res$Wähler),
-    Anteil_Briefwähler = round(sum(res$Wähler) / sum(res$Wahlberechtigte), 3) *
-      100
-  )
-  summary = as.data.frame(summary)
-  #res = rbind(res, summary)
-  return(res)
-}
-
 briefwähler_länder17 = briefwähler_länder(kreis17_zweitstimmen)
 briefwähler_länder17$Jahr = 2017
 briefwähler_länder21 = briefwähler_länder(kreis21_zweitstimmen)
@@ -195,7 +66,6 @@ briefwähler_länder_gesamt = rbind(briefwähler_länder17,
                                   briefwähler_länder21,
                                   briefwähler_länder25)
 
-# cleanup bund daten
 bund = bundDatenBereinigen(bund17, bund21, bund25)
 bund_komplett = bund[!grepl("Summe", bund$Bezirksart), ]
 bund_komplett = bund_komplett[!grepl("Summe", bund_komplett$Geschlecht), ]
@@ -217,57 +87,8 @@ savecsv(bund3, "Bund_sums.csv")
 kreis_daten_gesamt = kreisdatenBereinigen(kreis17_zweitstimmen,
                                           kreis21_zweitstimmen,
                                           kreis25_zweitstimmen)
+
 savecsv(kreis_daten_gesamt, "Kreisdaten_Gesamt.csv")
-
-# ANALYSEN
-kreisWahlberechtigteByGroup = function(filter_list) {
-  df = kreis_daten_gesamt
-  
-  for (col in names(filter_list)) {
-    val = filter_list[[col]]
-    df = df %>% filter(.data[[col]] == val)
-  }
-  
-  result = df %>%
-    arrange(`Wahlkreis-Nr.`) %>%
-    group_by(`Wahlkreis-Nr.`) %>%
-    slice_head(n = 1) %>%
-    ungroup()
-  
-  sum(result$Wahlberechtigte)
-}
-
-bundSumByGroup = function(filter_list) {
-  df = bund_komplett
-  for (col in colnames(filter_list)) {
-    val = filter_list[[col]]
-    df = df %>% filter(.data[[col]] == val)
-  }
-  return(sum(df$Summe))
-}
-
-bundAnalyse = function(..., group = "Jahr") {
-  args = list(...)
-  
-  res = bund_komplett
-  for (col in names(args)) {
-    val = args[[col]]
-    res = res %>% filter(.data[[col]] == val)
-  }
-  
-  res = res %>% group_by(across(all_of(group))) %>%
-    summarise(
-      Bezirksart_Anteil = pct(sum(Summe) / sum(bund_komplett[bund_komplett$Jahr == Jahr, "Summe"])),
-      CDU_CSU = pct((sum(CDU) + sum(CSU)) / bundSumByGroup(cur_group())),
-      SPD = pct(sum(SPD) / bundSumByGroup(cur_group())),
-      GRÜNE = pct(sum(GRÜNE) / bundSumByGroup(cur_group())),
-      LINKE = pct(sum(`DIE LINKE`) / bundSumByGroup(cur_group())),
-      FDP = pct(sum(FDP) / bundSumByGroup(cur_group())),
-      AFD = pct(sum(AfD) / bundSumByGroup(cur_group())),
-      Sonstige = pct(sum(Sonstige) / bundSumByGroup(cur_group()))
-    )
-  return(res)
-}
 
 res = bundAnalyse(group = c("Jahr", "Geschlecht"))
 # Wahlbeteiligung nach Wahlbezirksart in den Gruppen
@@ -280,5 +101,3 @@ res2 = kreis_daten_gesamt[, ] %>% group_by(across(all_of(filters))) %>%
     Wahlbeteiligung = pct(sum(Wähler) / kreisWahlberechtigteByGroup(cur_group())),
     
   )
-
-
