8a73c5350c1b8712d47a4fec26fc408bbb324a1b
Philipp Weingart <w2005@gmx.de>
2025-11-18 10:21:18 +0100

    first plotting

---
+++ r/plots.R
@@ -0,0 +1,47 @@
+library(tidyverse)
+library(sf)
+library(giscoR)
+library(stringdist)
+source("main.r")
+
+kreise = gisco_get_nuts(
+  country = "Germany",
+  nuts_level = 3,
+  year = "2024",
+  epsg = 3035,
+  resolution = "10"
+)
+
+kreiseReinigen = function(kreise) {
+  kreise %>%
+    tolower() %>%
+    gsub("andkreis|kreisfreie stadt|kreis|,|- ", "", .) %>%
+    trimws()
+}
+
+kreise$clean_name = kreiseReinigen(kreise$NAME_LATN)
+kreis_daten_gesamt$clean_name = kreiseReinigen(kreis_daten_gesamt$Wahlkreisname)
+
+matched_names = sapply(kreis_daten_gesamt$clean_name, function(name) {
+  which.min(stringdist::stringdist(name, kreise$clean_name, method = "jw"))
+})
+
+kreis_daten_gesamt$NUTS_ID = kreise$NUTS_ID[matched_names]
+kreis_daten_gesamt_sf = merge(kreise, kreis_daten_gesamt, by = "NUTS_ID")
+
+plot_kreis_daten = function(column, title, legend_title) {
+  ggplot(data = kreis_daten_gesamt_sf) +
+    geom_sf(aes_string(fill = column), color = "white", size = 0.2) +
+    scale_fill_viridis_c(option = "plasma", na.value = "grey90", name = legend_title) +
+    labs(title = title) +
+    theme_minimal() +
+    theme(
+      plot.title = element_text(hjust = 0.5, size = 16, face = "bold"),
+      legend.title = element_text(size = 12),
+      legend.text = element_text(size = 10)
+    )
+}
+
+plot1 = plot_kreis_daten("Wähler", "Anteil der Briefwähler nach Wahlkreis 2025", "Anteil der Briefwähler")
+# Unfinished 
+plot(plot1)
