89eff636647ca9df1dd04dce1733abccfc7d82a8
Philipp Weingart <w2005@gmx.de>
2025-11-24 14:24:13 +0100

    hab vergessen zu speichern

--- r/plots.R
+++ r/plots.R
@@ -4,7 +4,9 @@ library(giscoR)
 library(stringdist)
 source("main.r")
 
+# Anteil der Briefwähler von allen Wahlberechtigten
 wahlkreise = st_read("../data/wahlkreise/wahlkreise.shp")
+
 
 bundesländer = gisco_get_nuts(
   country = "Germany",
@@ -15,19 +17,23 @@ bundesländer = gisco_get_nuts(
 )
 
 #briefPlotLänder17 = left_join(bundesländer, länderArt[länderArt$Jahr == 2017 & länderArt$Wahlbezirksart == "Brief",], by = c("NUTS_NAME" = "Land"))
-briefPlotLänder = left_join(wahlkreise, länderArt[länderArt$Jahr == 2017 & länderArt$Wahlbezirksart == "Urne",], by = c("WKR_NR" = "Wahlkreis-Nr."))
+briefPlotLänder = left_join(wahlkreise, länderArt[länderArt$Jahr == 2025 & länderArt$Wahlbezirksart == "Brief",], by = c("WKR_NR" = "Wahlkreis-Nr."))
+briefPlotLänder$meistgewählt = factor(
+  briefPlotLänder$meistgewählt,
+  c("1", "2", "3", "4", "5", "6", "7"),
+  c("CDU", "CSU", "SPD", "LINKE", "GRÜNE", "FDP", "AFD")
+)
 
-wahlkreisPlot = ggplot(data = briefPlotLänder) + 
+ggplot(data = briefPlotLänder) + 
   geom_sf(aes(fill = (meistgewählt))) + 
   scale_fill_manual(
     name = "Partei",
-    values = c("1" = "black", "2" = "#343A40", "3" = "red", "4" = "pink", "5" = "green", "6" = "yellow", "7" = "lightblue"),
-    labels = c("CDU", "CSU", "SPD", "LINKE", "AFD")
+    values = c("CDU" = "black", "CSU" = "#343A40", "SPD" = "red", "LINKE" = "pink", "GRÜNE" = "green", "FDP" = "yellow", "AFD" = "lightblue"),
     ) +
   theme_minimal() +
   labs(
-    title = "Bundestagswahl 2017",
-    subtitle = "Urnenwähler"
+    title = "Bundestagswahl 2025",
+    subtitle = "Briefwähler"
   ) +
   theme(
     plot.title = element_text(hjust = 0.5, size = 16, face = "bold"),
@@ -35,7 +41,3 @@ wahlkreisPlot = ggplot(data = briefPlotLänder) +
     legend.title = element_text(hjust = 0.5, size = 12, face = "bold"),
     legend.text = element_text(size = 8),
   )
-
-plot(wahlkreisPlot)
-
-
