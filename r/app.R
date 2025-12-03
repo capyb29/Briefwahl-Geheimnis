7d54ef8b656709f225e054aa06b2b95292f5f727
Philipp Weingart <w2005@gmx.de>
2025-12-03 09:42:28 +0100

    ka warum der heult

--- r/app.R
+++ r/app.R
@@ -83,7 +83,7 @@ bbox = st_bbox(briefPlotLänder_longlat)
 
 ui = bootstrapPage(
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
-  leafletOutput("map", height = "100%", width = "100%"),
+  leaflet::leafletOutput("map", height = "100%", width = "100%"),
   absolutePanel(
     top = 10,
     width = "8vw",
