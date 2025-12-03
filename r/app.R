91285aa59629a34faf5c75f91d971489ba403cdb
Philipp Weingart <w2005@gmx.de>
2025-12-03 10:00:14 +0100

    hoffe das geht jtz bitte gott

--- r/app.R
+++ r/app.R
@@ -1,9 +1,12 @@
+require(leaflet)
 library(tidyverse)
 library(shiny)
 library(leaflet)
 library(stringdist)
 library(sf)
 library(htmltools)
+source("./main.r")
+
 
 # Anteil der Briefwähler von allen Wahlberechtigten
 wahlkreise = st_read("../data/wahlkreise/wahlkreise.shp")
@@ -83,7 +86,7 @@ bbox = st_bbox(briefPlotLänder_longlat)
 
 ui = bootstrapPage(
   tags$style(type = "text/css", "html, body {width:100%;height:100%}"),
-  leaflet::leafletOutput("map", height = "100%", width = "100%"),
+  leafletOutput("map", height = "100%", width = "100%"),
   absolutePanel(
     top = 10,
     width = "8vw",
